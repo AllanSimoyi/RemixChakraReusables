6b234a8ac8bce7f6f20aa911ff024c7b502cfa12
Allan Tatenda Simoyi <allansimoyi@gmail.com>
2023-01-14 19:14:25 +0200

    Update cloudinary rendering method

--- src/components/ImageUploadIcon.tsx
+++ src/components/ImageUploadIcon.tsx
@@ -1,24 +1,35 @@
-import { Spinner } from '@chakra-ui/react';
-import { AdvancedImage, placeholder } from "@cloudinary/react";
+import { Img, Spinner } from '@chakra-ui/react';
+import { thumbnail } from '@cloudinary/url-gen/actions/resize';
+import { useMemo } from 'react';
 import type { Icon as TablerIcon } from 'tabler-icons-react';
 import { PhotoPlus, X } from 'tabler-icons-react';
 import type { UploadState } from '../lib/cloudinary';
-import { cloudinaryImages } from '../lib/cloudinary';
 import { useCloudinary } from './CloudinaryContextProvider';
 
 type Props = React.ComponentProps<TablerIcon> & {
   status: UploadState;
   publicId: string;
+  boxSize?: number;
 }
 
 export function ImageUploadIcon (props: Props) {
-  const { status, publicId, ...otherProps } = props;
-  const { CLOUDINARY_CLOUD_NAME } = useCloudinary();
+  const { status, publicId, boxSize, ...otherProps } = props;
+  const { CloudinaryUtil } = useCloudinary();
+
   if (status === 'uploaded') {
+    const imageSrc = useMemo(() => {
+      return CloudinaryUtil
+        .image(publicId)
+        .resize(thumbnail().width(60).height(60))
+        .format('auto')
+        .quality('auto')
+        .toURL();
+    }, [publicId]);
     return (
-      <AdvancedImage
-        cldImg={cloudinaryImages(CLOUDINARY_CLOUD_NAME).getUploadThumbnail(publicId)}
-        plugins={[placeholder({ mode: 'pixelate' })]}
+      <Img
+        src={imageSrc}
+        width={boxSize || "60px"}
+        height={boxSize || "60px"}
       />
     );
   }
